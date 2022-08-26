667bcd3cb044d7e45b05b1c8fa70d1f1e5900424
antonmax29 <98175461+antonmax29@users.noreply.github.com>
2022-08-26 11:53:56 -0300

    tarefa - 4 em conclusao

--- tarefa-dia-4/lista-tarefa/script.js
+++ tarefa-dia-4/lista-tarefa/script.js
@@ -1,11 +1,12 @@
 console.log("ok")
 const inputTarefa = document.getElementById("tarefa-nova")
 
-
+let idTarefa = 0
 function addTarefa() {
     const item = document.createElement('li')
-  
-    item.innerHTML = `<li>
+  idTarefa++
+  item.id = idTarefa
+    item.innerHTML = `<li class="item">
     <span>${inputTarefa.value}</span>
     <button>x</button>
 </li>`
@@ -14,18 +15,20 @@ function addTarefa() {
     
     if(inputTarefa.value == ''){
         
-        alert( 'campo vazio!!')
-        alert(typeof(inputTarefa))
+        alert( 'campo vazio!! Digite um valor válido' )
+      //alert(typeof(inputTarefa))
 
-    }else if(inputTarefa.value == inputTarefa.value){
-    alert('ja existe tarefa')}
-    else{
+    }else{
         lista.appendChild(item)
     }
     
 
     
     
+}
+function concluir(itemId) {
+    const tarefaSelecionada = document.getElementById(itemId);
+    tarefaSelecionada.className = "concluida";
 }
 const botao = document.querySelector('#btn_adicionar')
 
