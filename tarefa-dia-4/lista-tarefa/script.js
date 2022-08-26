7abd4ee18bb3056037b3176cd5b7d25c47de53f8
antonmax29 <98175461+antonmax29@users.noreply.github.com>
2022-08-26 17:16:42 -0300

    tarefa dia 4 concluida

--- tarefa-dia-4/lista-tarefa/script.js
+++ tarefa-dia-4/lista-tarefa/script.js
@@ -1,35 +1,37 @@
-console.log("ok")
+
 const inputTarefa = document.getElementById("tarefa-nova")
 inputTarefa.focus() 
 let idTarefa = 0
-function addTarefa() {
-    const item = document.createElement('li')
-  idTarefa++
-  console.log(idTarefa)
-  item.id = idTarefa
 
-  const lista = document.querySelector('#lista')
+function addTarefa() {
+
+    const item = document.createElement('li')
+    idTarefa++
+    console.log(idTarefa)
+    item.id = idTarefa
+
+    const lista = document.querySelector('#lista')
+
     item.innerHTML = `<li class="item">
     <span onclick="concluir(${idTarefa})">${inputTarefa.value}</span>
     <button class="delete" onclick="excluir(${idTarefa})">x</button>
-</li>`
+    </li>`
 
-    
-    
     if(inputTarefa.value == ''){
         
         alert( 'campo vazio!! Digite a descrição da tarefa' )
       //alert(typeof(inputTarefa))
+      inputTarefa.focus() 
 
-    }else if(inputTarefa.value == item.firstChild.value){
-        alert('Já existe na lista')
-    } else{
+    }else
+     {
         lista.appendChild(item)
         inputTarefa.value = ''
         inputTarefa.focus()
     }
     
 }
+
 function concluir(itemId) {
     const tarefaSelecionada = document.getElementById(itemId);
     tarefaSelecionada.className = "concluida";
@@ -38,7 +40,9 @@ function concluir(itemId) {
 function excluir(itemExcluir) {
   const apagarItem = document.getElementById(itemExcluir);
         apagarItem.innerHTML = ''
+        inputTarefa.focus() 
 }
+
 const botao = document.querySelector('#btn_adicionar')
 
 botao.addEventListener('click', addTarefa)
