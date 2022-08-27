c5ce65e8fd52ceb040c6d38634fcb4728e785be6
antonmax29 <antssilva1992@gmail.com>
2022-08-27 12:46:15 -0300

    tarefa dia 4 concluida

--- tarefa-dia-4/lista-tarefa/script.js
+++ tarefa-dia-4/lista-tarefa/script.js
@@ -2,19 +2,21 @@
 const inputTarefa = document.getElementById("tarefa-nova")
 inputTarefa.focus() 
 let idTarefa = 0
+let selecao =0
+const btn_clear = document.getElementById('limpar')
 
 function addTarefa() {
 
     const item = document.createElement('li')
     idTarefa++
-    console.log(idTarefa)
+    
     item.id = idTarefa
 
     const lista = document.querySelector('#lista')
 
     item.innerHTML = `<li class="item">
-    <span onclick="concluir(${idTarefa})">${inputTarefa.value}</span>
-    <button class="delete" onclick="excluir(${idTarefa})">x</button>
+    <span ondblclick="removeConcluir(${idTarefa})"  onclick="concluir(${idTarefa})">${inputTarefa.value}</span>
+    <button onclick="excluir(${idTarefa})"><i class="fi fi-ss-trash"></i></i></button>
     </li>`
 
     if(inputTarefa.value == ''){
@@ -31,10 +33,18 @@ function addTarefa() {
     }
     
 }
-
+function removeConcluir(itemRemove){
+  const itemTirar = document.getElementById(itemRemove)
+  itemTirar.classList.remove("concluida")
+}
 function concluir(itemId) {
+
     const tarefaSelecionada = document.getElementById(itemId);
     tarefaSelecionada.className = "concluida";
+    
+  
+
+  
 }
 
 function excluir(itemExcluir) {
@@ -47,4 +57,9 @@ const botao = document.querySelector('#btn_adicionar')
 
 botao.addEventListener('click', addTarefa)
 
+btn_clear.addEventListener('click', limparLista)
 
+function limparLista() {
+  const lista = document.querySelector('#lista')
+  lista.innerHTML= ''
+}
